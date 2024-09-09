3e776d2bcc397aa1833848c9b9c605ecfca981c1
doctored11 <edit.shultz@yandex.ru>
2024-09-09 21:11:19 +0300

    web 2

--- src/components/logo/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-
-import styles from "./logo.module.css";
-import { Link } from "react-router-dom";
-
-export function Logo({ fontSize }: { fontSize?: number }){
-    return (<Link to = "/" className={styles.logo} style={{ fontSize: fontSize+"px"}}>CinemaGuide</Link>)
-}
